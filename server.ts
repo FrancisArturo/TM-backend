094cea720a4344d36f269c804ea5a02c6da111cf
Francis Arturo Alcalde <francisarturoalcaldesocole@gmail.com>
2025-08-29 18:53:29 -0300

    uploads route

--- server.ts
+++ server.ts
@@ -5,11 +5,19 @@ import authRoutes from './routes/authRoutes.ts';
 import userRoutes from './routes/userRoutes.ts';
 import taskRoutes from './routes/taskRoutes.ts';
 import reportRoutes from './routes/reportRoutes.ts';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 connectDB();
 
 app.use(cors(
@@ -27,6 +35,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/reports", reportRoutes);
+
+app.use("/uploads", express.static(path.join(__dirname,"uploads")));
 
 
 
